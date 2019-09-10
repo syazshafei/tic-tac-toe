856f97d0f931ba2c2747bb06a8c44e7dbc2a9053
Mohamad Syazwan Shafei <syazwan567@gmail.com>
2019-09-10 16:24:25 +0800

    Edited

--- src/index.js
+++ src/index.js
@@ -3,18 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
     render() {
       return (
         <button 
-        className="square" 
-        onClick={() => this.setState({value: 'X'})}>
-          {this.state.value}
+          className="square" 
+          onClick={() => this.props.onClick()}
+        >
+          {this.props.value}
         </button>
       );
     }
@@ -27,8 +22,20 @@ class Square extends React.Component {
         squares: Array(9).fill(null),
       };
     }
+
+    handleClick(i){
+      const squares = this.state.squares.slice();
+      squares[i] = 'X';
+      this.setState({squares: squares});
+    }
+    
     renderSquare(i) {
-      return <Square value={i}/>;
+      return (
+      <Square 
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+      );
     }
   
     render() {
